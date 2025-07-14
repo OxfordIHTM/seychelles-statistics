af84a85b71549fac4fa11e5eaede9174260b5e34
Ernest Guevarra <ernest@guevarra.io>
2025-07-14 08:47:48 +0100

    re-organise target names and output filenames fix #27; remove old/unused functions fix #30

--- _targets.R
+++ _targets.R
@@ -10,89 +10,98 @@ for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
 ## Download targets ----
 
-download_targets <- tar_plan(
+data_download_targets <- tar_plan(
   tar_target(
-    name = download_map_files,
+    name = map_download_files,
     command = download_unzip_maps(dest = "maps")
   ),
   ### Get download categories list of links ---- 
   tar_target(
-    name = download_categories_links,
+    name = categories_download_links,
     command = get_download_category_links(url = "https://www.nbs.gov.sc/downloads")
   ),
   ### Census data ----
   tar_target(
-    name = download_census_documents_links,
-    command = get_download_census_links(url = download_categories_links$url[1])
+    name = population_bulletin_download_links,
+    command = get_download_census_links(url = categories_download_links$url[1])
   ),
   tar_target(
-    name = download_census_documents_files,
+    name = population_bulletin_download_files,
     command = download_files_census(
-      download_census_documents_links,
+      population_bulletin_download_links,
       src = "Estimate|estimate|End|Mid|Abridged|Vital Statistics", 
       file_type = "pdf|PDF",
       path = "pdf"
     )
   ),
-  midyear_pop_census_files = get_mid_population_file_path(download_census_documents_files),
-  endyear_pop_census_files = get_end_population_file_path(download_census_documents_files),
-  midyear_pop_census_pages = c(8, 10, 10, 10, 11, 13, 11, 11),
-  midyear_pop_district_census_pages = c(14, 16, 17, 17, 18, 14, 14, 13),
-  endyear_deaths_pages = c(11, 14, 12, 14, 14, 16, 15, 18)
+  population_midyear_bulletin_files = get_mid_population_file_path(
+    population_bulletin_download_files
+  ),
+  population_endyear_bulletin_files = get_end_population_file_path(
+    population_bulletin_download_files
+  ),
+  population_midyear_bulletin_pages = c(8, 10, 10, 10, 11, 13, 11, 11),
+  population_midyear_bulletin_district_pages = c(14, 16, 17, 17, 18, 14, 14, 13),
+  deaths_endyear_pages = c(11, 14, 12, 14, 14, 16, 15, 18)
 )
 
 
 ## Data extraction targets ----
 
-data_targets <- tar_plan(
+data_extraction_targets <- tar_plan(
   tar_target(
     name = map_adm0,
     command = sf::st_read(
-      dsn = download_map_files, layer = "syc_admbnda_adm0_nbs2010"
+      dsn = map_download_files, layer = "syc_admbnda_adm0_nbs2010"
     )
   ),
   tar_target(
     name = map_adm1,
     command = sf::st_read(
-      dsn = download_map_files, layer = "syc_admbnda_adm1_nbs2010"
+      dsn = map_download_files, layer = "syc_admbnda_adm1_nbs2010"
     )
   ),
   tar_target(
     name = map_adm2,
     command = sf::st_read(
-      dsn = download_map_files, layer = "syc_admbnda_adm2_nbs2010"
+      dsn = map_download_files, layer = "syc_admbnda_adm2_nbs2010"
     )
   ),
   tar_target(
     name = map_adm3,
     command = sf::st_read(
-      dsn = download_map_files, layer = "syc_admbnda_adm3_nbs2010"
+      dsn = map_download_files, layer = "syc_admbnda_adm3_nbs2010"
     )
   ),
   tar_target(
-    name = midyear_pop_by_age_sex,
+    name = population_midyear_by_age_sex,
     command = extract_midyear_pop(
-      pdf = midyear_pop_census_files,
-      page = midyear_pop_census_pages
+      pdf = population_midyear_bulletin_files,
+      page = population_midyear_bulletin_pages
     ),
-    pattern = map(midyear_pop_census_files, midyear_pop_census_pages)
+    pattern = map(
+      population_midyear_bulletin_files, population_midyear_bulletin_pages
+    )
   ),
   tar_target(
-    name = midyear_pop_by_district,
+    name = population_midyear_by_district,
     command = extract_midyear_pop_district(
-      pdf = midyear_pop_census_files,
-      page = midyear_pop_district_census_pages,
+      pdf = population_midyear_bulletin_files,
+      page = population_midyear_bulletin_district_pages,
       ref_map = map_adm3
     ),
-    pattern = map(midyear_pop_census_files, midyear_pop_district_census_pages)
+    pattern = map(
+      population_midyear_bulletin_files, 
+      population_midyear_bulletin_district_pages
+    )
   ),
   tar_target(
-    name = endyear_deaths_by_age_sex,
+    name = deaths_endyear_by_age_sex,
     command = extract_endyear_deaths(
-      pdf = endyear_pop_census_files,
-      page = endyear_deaths_pages
+      pdf = population_endyear_bulletin_files,
+      page = deaths_endyear_pages
     ),
-    pattern = map(endyear_pop_census_files, endyear_deaths_pages)
+    pattern = map(population_endyear_bulletin_files, deaths_endyear_pages)
   )
 )
 
@@ -115,26 +124,26 @@ analysis_targets <- tar_plan(
 
 outputs_targets <- tar_plan(
   tar_target(
-    name = midyear_pop_by_age_sex_csv,
+    name = population_midyear_by_age_sex_csv,
     command = create_csv_data(
-      x = midyear_pop_by_age_sex,
-      dest = "data/population_by_age_sex.csv"
+      x = population_midyear_by_age_sex,
+      dest = "data/population_midyear_by_age_sex.csv"
     ),
     format = "file"
   ),
   tar_target(
-    name = midyear_pop_by_district_csv,
+    name = population_midyear_by_district_csv,
     command = create_csv_data(
-      x = midyear_pop_by_district,
-      dest = "data/population_by_district.csv"
+      x = population_midyear_by_district,
+      dest = "data/population_midyear_by_district.csv"
     ),
     format = "file"
   ),
   tar_target(
-    name = endyear_deaths_by_age_sex_csv,
+    name = deaths_endyear_by_age_sex_csv,
     command = create_csv_data(
-      x = endyear_deaths_by_age_sex,
-      dest = "data/deaths_by_age_sex.csv"
+      x = deaths_endyear_by_age_sex,
+      dest = "data/deaths_endyear_by_age_sex.csv"
     ),
     format = "file"
   )
@@ -144,8 +153,16 @@ outputs_targets <- tar_plan(
 ## Reports targets ----
 
 reports_targets <- tar_plan(
-  ##
+
 )
+
+
+## Handbook targets ----
+
+handbook_targets <- tar_plan(
+
+)
+
 
 ## Deploy targets ----
 
@@ -153,8 +170,10 @@ deploy_targets <- tar_plan(
 
 )
 
+
 ## Set seed
 set.seed(1977)
+
 
 ## Concatenate targets ----
 all_targets()
